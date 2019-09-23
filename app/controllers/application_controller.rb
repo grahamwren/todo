6862f067f219a899a961ac23bd9949fd655342bb
grahamwren <graham@maineisland.co>
2019-09-22 23:59:13 -0700

    Add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
+require 'token_manager'
+
+# Authentication handled with header: 'X-Access-Token'
 class ApplicationController < ActionController::API
+  attr_accessor :current_user
+
+  private
+
+  def authenticate_user
+    token = request.headers['X-Access-Token']
+    if (user_id = TokenManager.validate_token(token))
+      @current_user = User.find(user_id)
+    else
+      head :unauthorized
+      throw :halt
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, except: :login
+
+  def login
+    email, password = params.values_at :email, :password
+    if (user = User.find_by_email(email)&.authenticate(password))
+      render json: {
+        user_id: user.id,
+        access_token: TokenManager.generate_token(user.id)
+      }, status: 201
+    else
+      render status: :unauthorized
+    end
+  end
+
+  def show
+    if current_user.id == params[:id]
+      render json: current_user
+    else
+      render status: :forbidden
+    end
+  end
 end
